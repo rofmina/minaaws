62c66ca7e904419ea6bcede23cb7eb80848945b9
rofmina <119059458+rofmina@users.noreply.github.com>
2024-06-03 19:49:37 -0400

    Update s3bucket.tf

--- s3bucket.tf
+++ s3bucket.tf
@@ -1,12 +1,12 @@
 resource "aws_s3_bucket" "bucket1" {
-    count=2
 
-    tags = {
-        name="test-bucket-${count.index}"
-        }
-versioning_configuration {
-    status = "Disabled"
-  }
-
+  bucket = "example-bucket"
 }
 
+
+resource "aws_s3_bucket_versioning" "bucket1" {
+  bucket = aws_s3_bucket.bucket1.id
+  versioning_configuration {
+    status = "Disabled"
+  }
+}
