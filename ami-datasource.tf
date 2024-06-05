21b98fffaa381e0b7a1bd4209df8b8ef55d661fd
rofmina <119059458+rofmina@users.noreply.github.com>
2024-06-05 18:52:44 -0400

    Create ami-datasource.tf

---
+++ ami-datasource.tf
@@ -0,0 +1,24 @@
+data "aws_ami" "amz_linux2" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+}
