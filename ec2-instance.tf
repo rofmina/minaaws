db0988b0068bfba409b3734d89fe422b3f6c1dee
rofmina <119059458+rofmina@users.noreply.github.com>
2024-06-05 18:54:23 -0400

    Update ec2-instance.tf

--- ec2-instance.tf
+++ ec2-instance.tf
@@ -1,1 +1,9 @@
-
+resource "aws_instance""myec2m"{
+    ami=data.aws_ami.amz_linux2.id
+    instance_type = var.instance_type
+    vpc_security_group_ids = [aws_security_group.vpc-ssh.id,aws_security_group.vpc-web.id]
+    count = 2
+    tags = {
+        name="count-demo-${count.index}"
+    }
+}
